16bcd3a76f85d651fe4b76b1e367c05f55414d55
SimWEF <souarns@westinghouse.com>
2026-09-18 17:15:51 +0200

    amélioration recherche colisage

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
    Version : 3.0
    ========================================================== */
 
-   const APP_CACHE = "pigvs-app-v14";
+   const APP_CACHE = "pigvs-app-v15";
    const DATA_CACHE = "pigvs-data-v7";
    
    /* ==========================================================
