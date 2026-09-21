174f4f265745478e1bf510c85c1144b957063d52
SimWEF <souarns@westinghouse.com>
2026-09-21 16:24:24 +0200

    bugs saisie colisage

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@
    changer APP_CACHE pour voir un colisage mis a jour.
    ========================================================== */
 
-   const APP_CACHE = "pigvs-app-v38";
+   const APP_CACHE = "pigvs-app-v39";
    const DATA_CACHE = "pigvs-data-v14";
    
    /* ==========================================================
