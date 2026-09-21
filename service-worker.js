9206c525a0389a5f9ecb3eb13efd953998ad7a51
SimWEF <souarns@westinghouse.com>
2026-09-21 16:42:16 +0200

    EUh

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@
    changer APP_CACHE pour voir un colisage mis a jour.
    ========================================================== */
 
-   const APP_CACHE = "pigvs-app-v40";
+   const APP_CACHE = "pigvs-app-v41";
    const DATA_CACHE = "pigvs-data-v14";
    
    /* ==========================================================
