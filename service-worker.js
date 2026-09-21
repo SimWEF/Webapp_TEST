9ccaf84cac4d3c926037aa8d4fd31c6d4becaa5d
SimWEF <souarns@westinghouse.com>
2026-09-21 16:04:50 +0200

    amélioration saisie conteneur

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@
    changer APP_CACHE pour voir un colisage mis a jour.
    ========================================================== */
 
-   const APP_CACHE = "pigvs-app-v37";
+   const APP_CACHE = "pigvs-app-v38";
    const DATA_CACHE = "pigvs-data-v14";
    
    /* ==========================================================
